8510f54ecd2a2f0a3592b83d688af9d85c377687
syahrulpangestu <syahrulpangestu11@gmail.com>
2021-04-15 10:38:27 +0700

    move object & variable position to outside while

--- src/com/quizpbo/Main.java
+++ src/com/quizpbo/Main.java
@@ -2,15 +2,15 @@ package com.quizpbo;
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
         int key = 1;
+        int input_angka;
+        Scanner input = new Scanner(System.in);
+        Hitung hitung = new Hitung();
 
         while (key == 1) {
-            Scanner input = new Scanner(System.in);
             System.out.print("masukan angka : ");
-            int input_angka = input.nextInt();
-            Hitung hitung = new Hitung();
+            input_angka = input.nextInt();
 
             System.out.print("nilai faktorial " + input_angka + " adalah : " + hitung.faktorial(input_angka));
             System.out.print("\n\npress 1 to retry and any key to stop...");
